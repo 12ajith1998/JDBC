731db05b4e49cd94126d75bbb716621e4b8d1080
AJITH KUMAR A <12ajith1998@gmail.com>
2021-06-11 19:55:48 +0530

    Update and rename Sql.java to Sql1.java

--- Sql1.java
+++ Sql1.java
@@ -4,7 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class Sql 
+public class Sql1
 {
 	public static void main(String[] args) throws Exception
 	{
